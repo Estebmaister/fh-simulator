d21f725968ea9f6a0407c07f0cd29e112c0215e0
Esteban Camargo <estebmaister@gmail.com>
2021-11-03 10:34:13 -0500

    Creating a loader for waiting time

---
+++ js/resultCall.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    setTimeout(function(){
+        var scriptElement = document.createElement('script');
+        scriptElement.type = 'text/javascript';
+        if window.
+        scriptElement.src = "./js/bundle.js";
+        document.head.appendChild(scriptElement);
+    }, 2000);
+};
