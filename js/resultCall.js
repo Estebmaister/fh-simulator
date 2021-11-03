c226af8ac07587e242011081ef5d9e0bbd877378
Esteban Camargo <estebmaister@gmail.com>
2021-11-03 13:33:44 -0500

    Creating language switch

--- js/resultCall.js
+++ js/resultCall.js
@@ -2,8 +2,7 @@ window.onload = function(){
     setTimeout(function(){
         var scriptElement = document.createElement('script');
         scriptElement.type = 'text/javascript';
-        if window.
-        scriptElement.src = "./js/bundle.js";
+        scriptElement.src = "../js/bundle.js";
         document.head.appendChild(scriptElement);
     }, 2000);
 };
