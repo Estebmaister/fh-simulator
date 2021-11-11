4f541d22625dd6655c1fb8b7047ffcb20046f9fa
Esteban Camargo <estebmaister@gmail.com>
2021-11-10 23:15:33 -0500

    Starting dimensions config

--- data/update.js
+++ data/update.js
@@ -4,14 +4,28 @@
 	const dataPaths = {
 		csv: __dirname + "/data.csv",
 		json: __dirname + "/data.json"
-	}  
+	}
+	const dimensionsPaths = {
+		csv: __dirname + "/dimensions.csv",
+		json: __dirname + "/dimensions.json"
+	}
 	console.log(`{"INFO": "Starting data extraction for fh-simulator"}`);
 	const CompoundsArray = parse(fs.readFileSync(dataPaths.csv), {
 		columns: true,
 		skip_empty_lines: true,
-		cast: true
+		cast: true,
+		trim: true
 	})
-	
 	fs.writeFileSync(dataPaths.json, JSON.stringify(CompoundsArray, null, 2))
 	console.log(`{"INFO": "JSON file successfully updated"}`);
+
+	const Dimensions = parse(fs.readFileSync(dimensionsPaths.csv), {
+		columns: true,
+		skip_empty_lines: true,
+		cast: true,
+		trim: true,
+		relax_column_count: true
+	})
+	
+	fs.writeFileSync(dimensionsPaths.json, JSON.stringify(Dimensions, null, 2))
 })();
