f00e6307bfec947f9fb6ff0a262ba69e80b7c6a9
Restyled.io <commits@restyled.io>
2022-07-08 06:02:50 +0000

    Restyled by prettier

--- js/browserResults/graphicData.js
+++ js/browserResults/graphicData.js
@@ -1,110 +1,111 @@
-const {logger, unitConv} = require('./../utils');
+const { logger, unitConv } = require("./../utils");
 
-const graphicData =
-    (comb, fuel, opt) => {
-      // Shut down loggers to avoid console overload during graphic drawing
-      const savedLogger = {...logger};
-      logger.error = () => 0;
-      logger.default = () => 0;
-      logger.info = () => 0;
-      logger.debug = () => 0;
+const graphicData = (comb, fuel, opt) => {
+  // Shut down loggers to avoid console overload during graphic drawing
+  const savedLogger = { ...logger };
+  logger.error = () => 0;
+  logger.default = () => 0;
+  logger.info = () => 0;
+  logger.debug = () => 0;
 
-      opt.unitSystem = 'english'
-      const browserResult = [];
+  opt.unitSystem = "english";
+  const browserResult = [];
 
-      // Checking local storage to avoid repeating calculations
-      let prevResult;
-      let localResult = JSON.parse(localStorage.getItem(`${opt.title}_graph`));
-      if (localResult) {
-        prevResult = localResult[`${window.location.search}`];
-        if (Object.keys(localResult).length > 5)
-          localStorage.clear();
-      }
-      if (prevResult) {
-        draw(prevResult, opt);
-        return;
-      }
+  // Checking local storage to avoid repeating calculations
+  let prevResult;
+  let localResult = JSON.parse(localStorage.getItem(`${opt.title}_graph`));
+  if (localResult) {
+    prevResult = localResult[`${window.location.search}`];
+    if (Object.keys(localResult).length > 5) localStorage.clear();
+  }
+  if (prevResult) {
+    draw(prevResult, opt);
+    return;
+  }
 
-      let graphVar;
-      opt.graphRange *= 1e-2;
-      switch (opt.graphVar) {
-      case 'humidity':
-        graphVar = 'humidity';
-        break;
-      case 'air_excess':
-        graphVar = 'airExcess';
-        break;
-      case 'o2_excess':
-        graphVar = 'o2Excess';
-        break;
-      case 'm_fluid':
-        graphVar = 'mFluid';
-        opt.graphRange *= 1e5;
-        break;
-      default:
-        graphVar = 'tOut';
-        opt.graphRange *= 1e2;
-        break;
-      }
+  let graphVar;
+  opt.graphRange *= 1e-2;
+  switch (opt.graphVar) {
+    case "humidity":
+      graphVar = "humidity";
+      break;
+    case "air_excess":
+      graphVar = "airExcess";
+      break;
+    case "o2_excess":
+      graphVar = "o2Excess";
+      break;
+    case "m_fluid":
+      graphVar = "mFluid";
+      opt.graphRange *= 1e5;
+      break;
+    default:
+      graphVar = "tOut";
+      opt.graphRange *= 1e2;
+      break;
+  }
 
-      const points = opt.graphPoints, range = opt.graphRange;
-      let initVar = opt[graphVar] - range / 2;
-      if (initVar < 0)
-        initVar = 0;
-      savedLogger.info(`Var: ${graphVar}, centerValue: ${
-          opt[graphVar]}, range: ${range}, points: ${points}`);
-      for (let index = 0; index < points; index++) {
-        opt[graphVar] = initVar + index * range / points;
-        const runResult = comb(fuel, opt);
-        browserResult[index] = {
+  const points = opt.graphPoints,
+    range = opt.graphRange;
+  let initVar = opt[graphVar] - range / 2;
+  if (initVar < 0) initVar = 0;
+  savedLogger.info(
+    `Var: ${graphVar}, centerValue: ${opt[graphVar]}, range: ${range}, points: ${points}`
+  );
+  for (let index = 0; index < points; index++) {
+    opt[graphVar] = initVar + (index * range) / points;
+    const runResult = comb(fuel, opt);
+    browserResult[index] = {
+      // --- Input vars
+      m_fluid:
+        unitConv.lb_htoBPD(unitConv.kgtolb(runResult.rad_result.m_fluid)) *
+        1e-3,
+      t_out: unitConv.KtoF(runResult.rad_result.t_out),
 
-          // --- Input vars
-          m_fluid : unitConv.lb_htoBPD(
-                        unitConv.kgtolb(runResult.rad_result.m_fluid)) *
-                        1e-3,
-          t_out : unitConv.KtoF(runResult.rad_result.t_out),
+      o2_excess: runResult.flows["O2_%"] < 11 ? runResult.flows["O2_%"] : 11,
+      air_excess:
+        runResult.flows["air_excess_%"] > 0
+          ? runResult.flows["air_excess_%"]
+          : 0,
+      humidity: runResult.debug_data["humidity_%"],
 
-          o2_excess : runResult.flows['O2_%'] < 11 ? runResult.flows['O2_%']
-                                                   : 11,
-          air_excess : runResult.flows['air_excess_%'] > 0
-                           ? runResult.flows['air_excess_%']
-                           : 0,
-          humidity : runResult.debug_data['humidity_%'],
+      // --- Output vars
+      cnv_tg_out: unitConv.KtoF(runResult.conv_result.tg_out),
 
-          // --- Output vars
-          cnv_tg_out : unitConv.KtoF(runResult.conv_result.tg_out),
+      // m_flue:     runResult.shld_result.m_flue ?
+      // runResult.shld_result.m_flue : 0,
+      m_fuel: runResult.rad_result.m_fuel
+        ? unitConv.kgtolb(runResult.rad_result.m_fuel)
+        : 0,
+      efficiency: runResult.rad_result.eff_thermal_val
+        ? runResult.rad_result.eff_thermal_val
+        : 0,
+      // duty_total: runResult.rad_result.duty_total,
+      rad_dist:
+        runResult.rad_result["%"] < 1
+          ? Math.round(1e5 * runResult.rad_result["%"]) / 1e3
+          : 0,
+      cnv_dist:
+        runResult.conv_result["%"] < 1
+          ? Math.round(1e5 * runResult.conv_result["%"]) / 1e3
+          : 0,
+      // shl_duty:  runResult.shld_result.duty,
+      co2_emiss:
+        Math.round(
+          1e2 *
+            runResult.products["CO2"] *
+            (44.01 / runResult.flows["fuel_MW"]) *
+            runResult.rad_result.m_fuel *
+            (1e-3 * 24 * 365)
+        ) / 1e2,
+    };
+  }
 
-          // m_flue:     runResult.shld_result.m_flue ?
-          // runResult.shld_result.m_flue : 0,
-          m_fuel : runResult.rad_result.m_fuel
-                       ? unitConv.kgtolb(runResult.rad_result.m_fuel)
-                       : 0,
-          efficiency : runResult.rad_result.eff_thermal_val
-                           ? runResult.rad_result.eff_thermal_val
-                           : 0,
-          // duty_total: runResult.rad_result.duty_total,
-          rad_dist : runResult.rad_result['%'] < 1
-                         ? Math.round(1e5 * runResult.rad_result['%']) / 1e3
-                         : 0,
-          cnv_dist : runResult.conv_result['%'] < 1
-                         ? Math.round(1e5 * runResult.conv_result['%']) / 1e3
-                         : 0,
-          // shl_duty:  runResult.shld_result.duty,
-          co2_emiss :
-              Math.round(1e2 * runResult.products["CO2"] *
-                         (44.01 / runResult.flows["fuel_MW"]) *
-                         runResult.rad_result.m_fuel * (1e-3 * 24 * 365)) /
-                  1e2,
+  if (!localResult) localResult = {};
+  localResult[`${window.location.search}`] = browserResult;
+  localStorage.setItem(`${opt.title}_graph`, JSON.stringify(localResult));
+  console.log(browserResult);
+  draw(browserResult, opt);
+};
 
-        }
-      }
-
-      if (!localResult)
-        localResult = {};
-      localResult[`${window.location.search}`] = browserResult;
-      localStorage.setItem(`${opt.title}_graph`, JSON.stringify(localResult));
-      console.log(browserResult);
-      draw(browserResult, opt);
-    }
-
-                         module.exports = {graphicData};
+module.exports = { graphicData };
